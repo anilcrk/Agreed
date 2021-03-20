b6f1c108a0f27d0a63bdc1a8064f20f8f0c78c9b
Anil <anilcirik37@gmail.com>
2021-03-20 22:05:39 +0300

    Generic Repository Pattern and UnitOfWork

---
+++ Agreed.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using Agreed.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agreed.Core.Repositories
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+    }
+}
---
+++ Agreed.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Agreed.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agreed.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IOrderRepository Orders { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
