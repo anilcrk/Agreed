6aba7ddcd156adb39f8b3e3ab8a88c0719fa56c7
Anil <anilcirik37@gmail.com>
2021-05-09 18:01:59 +0300

    added seeddata

---
+++ Agreed.DataAccess/Seed/CompanySeed.cs
@@ -0,0 +1,22 @@
+﻿using Agreed.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agreed.DataAccess.Seed
+{
+    class CompanySeed : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasData(
+                new Company
+                {
+                    Id = 1,
+                    CompanyName = "General Company"
+                });
+        }
+    }
+}
---
+++ Agreed.DataAccess/Seed/UserSeed.cs
@@ -0,0 +1,26 @@
+﻿using Agreed.Core.Entities;
+using Agreed.Core.Helpers.Cryption;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agreed.DataAccess.Seed
+{
+    class UserSeed : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(new User
+            {
+                CompanyId = 1,
+                Email = AES256.EncryptAndEncode("test@gmail.com"),
+                Password = AES256.EncryptAndEncode("test"),
+                FirstName = "test",
+                LastName = "test",
+                Status = true
+            });
+        }
+    }
+}
