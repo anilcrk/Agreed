9e61a58c539f9e232cf6cd70d710264b77f0bf47
Anil <anilcirik37@gmail.com>
2021-05-15 16:52:55 +0300

    server side filtering added

--- Agreed.WebUI/Controllers/AgreedController.cs
+++ Agreed.WebUI/Controllers/AgreedController.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
+using Agreed.Core.Entities;
 using Agreed.WebUI.Models.ViewModels;
 using Agreed.WebUI.ModelServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq.Dynamic.Core;
+using Agreed.Core.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using Agreed.DataAccess;
 
 namespace Agreed.WebUI.Controllers
 {
@@ -13,11 +19,15 @@ namespace Agreed.WebUI.Controllers
     {
         private readonly AgreedModelService _modelService;
         private readonly int _userCompanyId;
+        private readonly DbContext _dbContext;
+        public readonly DbSet<Order> _orderDbSet;
 
-        public AgreedController(AgreedModelService modelService, IHttpContextAccessor contextAccessor)
+        public AgreedController(AgreedModelService modelService, IHttpContextAccessor contextAccessor, AppDbContext dbContext)
         {
             _modelService = modelService;
             _userCompanyId = Convert.ToInt32(contextAccessor.HttpContext.User.Claims.FirstOrDefault(c => c.Type == Core.Enums.ClaimTypes.CompanyId.ToString()).Value);
+            _dbContext = dbContext;
+            _orderDbSet = dbContext.Set<Order>();
         }
 
         public async Task<IActionResult> Index()
@@ -28,6 +38,40 @@ namespace Agreed.WebUI.Controllers
             model.Orders = orders;
 
             return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult GetAgreedItems()
+        {
+            var pageSize = int.Parse(Request.Form["length"]);
+            var skip = int.Parse(Request.Form["start"]);
+
+            var searchValue = Request.Form["search[value]"];
+
+            var sortColumn = Request.Form[string.Concat("columns[", Request.Form["order[0][column]"], "][name]")];
+            var sortColumnDirection = Request.Form["order[0][dir]"];
+
+            //var orders = await _modelService.GetAgrrededOrders(_userCompanyId);
+
+            IQueryable<Order> ordersFilter = _orderDbSet.Where(m => string.IsNullOrEmpty(searchValue)
+                ? true
+                : (m.Barcode.Contains(searchValue) 
+                || m.ProductName.Contains(searchValue) 
+                || m.PackageNumber.Contains(searchValue) 
+                || m.Email.Contains(searchValue)) && m.CompanyId == _userCompanyId).ToList().AsQueryable();
+
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+            {
+                ordersFilter = ordersFilter.OrderBy(string.Concat(sortColumn, " ", sortColumnDirection));
+            }
+
+            var data = ordersFilter.Skip(skip).Take(pageSize).ToList();
+
+            var recordsTotal = ordersFilter.Count();
+
+            var jsonData = new { recordsFiltered = recordsTotal, recordsTotal, data };
+
+            return Ok(jsonData);
         }
     }
 }
---
+++ Agreed.WebUI/wwwroot/js/AgreedDataTables.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+    $('#agreedTable').dataTable({
+        "processing": true,
+        "serverSide": true,
+        "filter": true,
+        "ajax": {
+            "url": "/Agreed/GetAgreedItems",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "columnDefs": [{
+            "targets": [0],
+            "visible": false,
+            "searchable": false
+        }],
+        "columns": [
+            { "data": "id", "name": "Id", "autowidth": true },
+            { "data": "barcode", "name": "Barcode", "autowidth": true },
+            { "data": "packageNumber", "name": "PackageNumber", "autowidth": true },
+            { "data": "cargoCompany", "name": "CargoCompany", "autowidth": true },
+            { "data": "orderDate", "name": "OrderDate", "autowidth": true },
+            //{
+            //    "render": function (data, type, row) { return '<span>' + row.dateOfBirth.split('T')[0] + '</span>' },
+            //    "name": "OrderDate"
+            //},
+            {
+                "render": function (data, type, row) { return '<a href="#" class="btn btn-danger" onclick=DeleteCustomer("' + row.id + '"); > Delete </a>' },
+                "orderable": false
+            },
+
+        ]
+    });
+});
